29f0d6473b9eaae454c35b5ab8a8c9211b5fd406
Anupriya <anupriyanatarajan2007@gmail.com>
2026-03-15 21:44:04 +0530

    Initial commit for Todo full stack project

---
+++ todo-backend/demo/src/main/java/com/example/demo/model/Todo.java
@@ -0,0 +1,42 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private boolean completed;
+
+    public Todo() {}
+
+    public Todo(String title, boolean completed) {
+        this.title = title;
+        this.completed = completed;
+    }
+
+
+    public void setId(Long id) { this.id = id; }
+
+    public Long getId() { return id; }
+
+    public void setTitle(String title) { this.title = title; }
+
+    public String getTitle() { return title; }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+
+    public boolean isCompleted() { return completed; }
+
+   
+}
